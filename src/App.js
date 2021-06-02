49ea2b14acf16ce85ef2ab469a897986f1890638
GromovVv <pavel.kuts@gmail.com>
2021-06-03 00:22:44 +0300

    ready

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
+import { useEffect } from 'react';
+import { fetchContacts } from './Redux/phonebook-operations';
 import PropTypes from 'prop-types';
 import { getVisibleContacts, getLoading } from './Redux/phonebook-selectors';
 import ContactForm from './Components/ContactForm';
 import Filter from './Components/Filter';
 import ContactList from './Components/ContactList';
 import { useSelector, connect } from 'react-redux';
-
 import 'modern-normalize/modern-normalize.css';
 import './App.scss';
-
-function App({isLoading}) {
+function App({ fetchContacts, isLoading }) {
   const contacts = useSelector(getVisibleContacts);
   const totalContactsCount = contacts.length;
-
+  useEffect(() => {
+    fetchContacts();
+  }, [fetchContacts]);
   return (
     <div className="App">
-      { isLoading && <h1>Loading...</h1>}
+      {isLoading && <h1>Loading...</h1>}
       <h2 className="Title">Phonebook</h2>
       <ContactForm />
       <h2 className="Title">Total contacts: {totalContactsCount}</h2>
       <Filter />
       {contacts.length > 0 ? (
-          <ContactList />
+        <ContactList />
       ) : (
-        <span className="Empty">Your phonebook is empty or you have no such contact</span>
+        <span className="Empty">
+          Your phonebook is empty or you have no such contact
+        </span>
       )}
     </div>
   );
 }
-
 App.propTypes = {
   filter: PropTypes.string,
   contacts: PropTypes.arrayOf(
@@ -38,9 +41,10 @@ App.propTypes = {
     }),
   ),
 };
-
 const mapStateToProps = state => ({
   isLoading: getLoading(state),
 });
-
-export default connect(mapStateToProps)(App)
+const mapDispatchToProps = dispatch => ({
+  fetchContacts: () => dispatch(fetchContacts()),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/Components/ContactList/ContactList.js
+++ src/Components/ContactList/ContactList.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { useDispatch, connect } from 'react-redux';
 import { deleteContact, fetchContacts } from '../../Redux/phonebook-operations';
 import { getVisibleContacts } from '../../Redux/phonebook-selectors';
@@ -7,7 +6,6 @@ import PropTypes from 'prop-types';
 import './ContactList.scss';
 
 function ContactList({contacts}) {
-  useEffect(() => { dispatch(getVisibleContacts) });
   const dispatch = useDispatch();
   const onDeleteContact = id => dispatch(deleteContact(id));
 
@@ -45,30 +43,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchContacts: () => console.log(dispatch(fetchContacts())),
+  fetchContacts: () => dispatch(fetchContacts()),
   onDeleteContact: id => dispatch(deleteContact(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
-
-// render() {
-//   const { contacts, onDeleteContact } = this.props;
-// //   const contacts = useSelector(getVisibleContacts);
-// // const dispatch = useDispatch();
-//   return (
-//     <ul className="Contact__list">
-//       {contacts.map(({ id, name, number }) => (
-//         <li key={id} className="ContactList__item">
-//           {name}: {number}
-//           <button
-//             type="button"
-//             onClick={() => onDeleteContact(id)}
-//             className="ContactList__btn"
-//           >
-//             Delete
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
